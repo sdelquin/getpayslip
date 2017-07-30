af47996267e63c3a2e3a1ff3feab611154534620
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-07-30 13:56:39 +0100

    Fixed path in fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,6 @@ env.hosts = ["production"]
 def deploy():
     local("git push")
     with prefix("source ~/.virtualenvs/getpayslip/bin/activate"):
-        with cd("~getpayslip"):
+        with cd("~/getpayslip"):
             run("git pull")
             run("pip install -r requirements.txt")
