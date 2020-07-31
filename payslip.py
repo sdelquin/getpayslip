19215d63818bc8d92fbd12ccb82d097625481586
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-07-31 11:26:08 +0100

    Fix download options for geckodriver in order to retrieve the payslip

--- payslip.py
+++ payslip.py
@@ -17,26 +17,36 @@ from log import init_logger
 logger = init_logger(__file__)
 
 
-class PaySlip():
+class PaySlip:
     def __init__(self, month, year, headless=True):
         logger.info('START: Building PaySlip object')
         self.month, self.year = month, year
         self._init_webdriver(headless)
 
     def _init_webdriver(self, headless):
-        # selenium configurations
+        # Selenium configurations
         options = Options()
         options.headless = headless
 
         profile = webdriver.FirefoxProfile()
+        # Controls the default folder to download a file to. 0 indicates the Desktop;
+        # 1 indicates the systems default downloads location; 2 indicates a custom folder.
         profile.set_preference('browser.download.folderList', 2)
+        # Holds the custom destination folder for downloading
         profile.set_preference('browser.download.dir', os.getcwd())
-        profile.set_preference('browser.helperApps.neverAsk.saveToDisk',
-                               'application/pdf')
-        profile.set_preference('pdfjs.migrationVersion', 1)
+        # stores a comma-separated list of MIME types to save to disk without asking
+        # what to use to open the file
+        profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/pdf')
+        # Prevent Firefox from previewing PDFs
+        profile.set_preference('pdfjs.disabled', True)
+        # Needs to be false, so that Firefox won't scan and load plugins
+        profile.set_preference('plugin.scan.plid.all', False)
+        # Is a key that holds the minimum allowed version number that Adobe Acrobat
+        # is allowed to launch. Setting it to a number larger than currently installed
+        # Adobe Acrobat version should do the trick.
+        profile.set_preference('plugin.scan.Acrobat', '99.0')
 
-        self.driver = webdriver.Firefox(
-            options=options, firefox_profile=profile)
+        self.driver = webdriver.Firefox(options=options, firefox_profile=profile)
 
     def __del__(self):
         self.driver.quit()
@@ -60,7 +70,8 @@ class PaySlip():
         try:
             self.driver.get(config.PAYSLIP_URL)
             WebDriverWait(self.driver, config.TIME_OUT).until(
-                EC.presence_of_element_located((By.ID, 'btn-login')))
+                EC.presence_of_element_located((By.ID, 'btn-login'))
+            )
         except TimeoutException:
             logger.error('Timeout waiting for page loading')
             return False
@@ -75,13 +86,16 @@ class PaySlip():
         try:
             element.click()
             payslip_button = WebDriverWait(self.driver, config.TIME_OUT).until(
-                EC.presence_of_element_located((By.NAME, 'ultima')))
+                EC.presence_of_element_located((By.NAME, 'ultima'))
+            )
         except TimeoutException:
             logger.error('Timeout waiting for page loading')
             return False
-        element = self.driver.find_element_by_xpath('//*[@id=\
+        element = self.driver.find_element_by_xpath(
+            '//*[@id=\
 "principal_interior"]/table/tbody/tr[3]/td/table/tbody/tr/td[3]/table/tbody/\
-tr/td[2]/table/tbody/tr/td/table[9]/tbody/tr[2]/td[3]')
+tr/td[2]/table/tbody/tr/td/table[9]/tbody/tr[2]/td[3]'
+        )
         month, year = date_utils.parse_date(element.text)
 
         if month == self.month and year == self.year:
@@ -105,15 +119,13 @@ tr/td[2]/table/tbody/tr/td/table[9]/tbody/tr[2]/td[3]')
     def send_payslip(self):
         logger.info('Sending email with attached downloaded payslip')
         email = SendGrid(
-            config.SENDGRID_APIKEY,
-            config.SENDGRID_FROM_EMAIL,
-            config.SENDGRID_FROM_NAME
+            config.SENDGRID_APIKEY, config.SENDGRID_FROM_EMAIL, config.SENDGRID_FROM_NAME
         )
         email.send(
             to=config.TO_EMAIL_ADDRESS,
             subject=f'Payslip {self}',
             msg="It's only money 💶💶💶 but I like it! 🐼",
-            attachments=self.filename
+            attachments=self.filename,
         )
 
     @staticmethod
