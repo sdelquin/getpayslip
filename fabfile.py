ff0db1e5ac9355b22d0d8b04ef4d2798002cb65f
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-08 12:36:32 +0000

    Fix virtualenv base path

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ env.hosts = ['sdelquin.me']
 
 def deploy():
     local('git push')
-    with prefix('source ~/.virtualenvs/getpayslip/bin/activate'):
+    with prefix('source ~/.pyenv/versions/getpayslip/bin/activate'):
         with cd('~/code/getpayslip'):
             run('git pull')
             run('pip install -r requirements.txt')
